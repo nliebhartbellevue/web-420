b79ed6c5bb8ac8df5a6abb0f25ca1ca86d0df76a
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2019-10-21 15:06:19 -0500

    config.js

---
+++ api-gateway/config.js
@@ -0,0 +1,10 @@
+/**
+ * Title: config.js
+ * Author: Nathaniel Liebhart
+ * Date: October 21, 2019
+ * Description: This file will serve as a global hub for application level configurations
+ */
+var config = {};
+config.web = {};
+config.web.port = process.env.PORT || "3000";
+module.exports = config;
